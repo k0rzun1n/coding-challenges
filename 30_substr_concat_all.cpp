537ff4fa78b97b77f4a10f4a47e6c77f1f72c5e1
Sergey <korzuninsl@mail.ru>
2024-02-16 00:37:11 +0500

    30 string_view

--- 30_substr_concat_all.cpp
+++ 30_substr_concat_all.cpp
@@ -1,19 +1,20 @@
 #include "LeetCode.h"
 class Solution {
    public:
-    vector<int> findSubstring(string s, vector<string> &words) {
+    vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> res;
-        unordered_map<string, int> um_full;
-        for (auto w : words) um_full[w]++;
+        const char* str = s.c_str();
+        unordered_map<string_view, int> um_full;
+        for (string_view w : words) um_full[w]++;
         const int wlen = words[0].length();
         const int tl = words.size() * wlen;  // target len
         for (int k = 0; k < wlen; k++) {
             int ws = k;               // window start
             int zc = um_full.size();  // zero count
             auto um_window = um_full;
-
             for (int i = k; i < s.length(); i += wlen) {
-                string ss = s.substr(i, wlen);
+                // string_view ss = s.substr(i, wlen);
+                string_view ss (str + i, wlen);
                 if (um_full.find(ss) == um_full.end()) {
                     um_window = um_full;  // bad word, reset window
                     ws = i + wlen;
@@ -28,14 +29,14 @@ class Solution {
                     if (!zc) {
                         res.push_back(ws);
                     }
-                    ss = s.substr(ws, wlen);
+                    // ss = s.substr(ws, wlen);
+                    ss = string_view (str + ws, wlen);
                     cur = um_window[ss];
                     next = ++um_window[ss];
                     if (next == 0) zc--;
                     if (cur == 0) zc++;
                     ws += wlen;
                 }
-
             }
         }
         return res;
@@ -47,13 +48,13 @@ int main() {
 
     vector<string> v;
 
-    // v = {"foo", "bar"};
+    v = {"foo", "bar"};
     // v = {"bar"};
-    // cc, q.findSubstring("barfoothefoobarman", v);
+    cc, q.findSubstring("barfoothefoobarman", v);
     // v = {"word", "good", "best", "good"};
     // cc, q.findSubstring("wordgoodgoodgoodbestword", v);
     v = {"fooo", "barr", "wing", "ding", "wing"};
-    cc, q.findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake",v);
+    cc, q.findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", v);
     v = {"aa", "aa"};
     cc, q.findSubstring("aaaaaaaaaaaaaa", v);
     // Output
