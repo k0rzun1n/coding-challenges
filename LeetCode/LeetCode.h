79163dfd6a37f71df895628a680b112860ab3c62
k0rzun1n <korzuninsl@mail.ru>
2024-12-13 12:47:37 +0500

    Update LeetCode.h
    ListNode and makeList

--- LeetCode/LeetCode.h
+++ LeetCode/LeetCode.h
@@ -23,6 +23,23 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
 struct cd {
     template <typename T>
     cd& operator,(const T& v) {
@@ -78,35 +95,40 @@ struct cd {
         cout << "] " << endl;
         return *this;
     }
+    // int list
+    cd& operator,(ListNode* start) {
+        cout << "[  ";
+        for (; start; start = start->next) {
+            cout << start->val;
+            cout << "  ";
+        }
+        cout << "] " << endl;
+        return *this;
+    }
 
     cd& operator~() {
         cout << endl;
         return *this;
     }
     cd& operator!() {
-        cout << endl << "-------------------------" << endl;
+        cout << endl
+             << "-------------------------" << endl;
         return *this;
     }
     ~cd() { cout << "\n"; }
 };
 auto cc = cd();
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right)
-        : val(x), left(left), right(right) {}
-};
+ListNode* makeList(vector<int> data) {
+    if (data.empty()) return nullptr;
+    auto it = data.begin();
+    ListNode* start = new ListNode(*it);
+    ListNode* tmp = start;
+    while (++it != data.end())
+        tmp = (tmp->next = new ListNode(*it));
+    tmp->next = nullptr;
+    return start;
+}
 
 TreeNode* makeTree(vector<int> data) {
     queue<TreeNode*> qNode;
@@ -150,3 +172,22 @@ uint32_t bitCount(uint32_t n) {
     n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
     return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
 }
+
+class range {
+public:
+    struct rangeIt{
+        rangeIt(int v) : _v(v){}
+        int operator*()const{return _v;}
+        void operator++(){_v++;}
+        bool operator!=(const rangeIt & other)const{ return _v != other._v;}
+    private:
+        int _v;
+    };
+    range(int a, int b):_a(a),_b(b){}
+    rangeIt begin() const { return rangeIt(_a); }
+    rangeIt end() const { return rangeIt(_b); }
+private:
+    int _a, _b;
+};
+// for(int i : range(0, 100)) 
+//     printf("%d\n", i);
