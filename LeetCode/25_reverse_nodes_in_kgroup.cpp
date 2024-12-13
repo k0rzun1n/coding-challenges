9d880d42d8bf1eac14597b038ed11eb10e2841a0
k0rzun1n <korzuninsl@mail.ru>
2024-12-13 12:49:16 +0500

    Update 25_reverse_nodes_in_kgroup.cpp

--- LeetCode/25_reverse_nodes_in_kgroup.cpp
+++ LeetCode/25_reverse_nodes_in_kgroup.cpp
@@ -1,24 +1,38 @@
 #include "LeetCode.h"
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-public:
+   public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        //TODO
+        if (k == 1) return head;
+        ListNode *a, *b, *c, *d;
+        a = b = head;
+        c = d = nullptr;
+        int len = 0;
+        while (head) head = head->next, len++;
+        for (int i = 0; i < len / k; i++) {
+            for (int j = 0; j < k - 1; j++) b = b->next;
+            if (d) d->next = b;
+            b = b->next;
+            d = a;
+            for (int j = 0; j < k; j++) {
+                c = a->next;
+                a->next = b;
+                b = a;
+                a = c;
+            }
+            if (!i) head = b;
+            b = c;
+        }
+        return head;
     }
 };
 
 int main() {
     Solution q;
+    vector<int> data{0, 1, 2, 3, 4, 5, 6, 7};
+    auto d = makeList(data);
+    // cc, d;
+    cc, q.reverseKGroup(makeList(data), 3);
 
     return 0;
 }
