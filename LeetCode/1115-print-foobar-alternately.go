288c0632be380006c569e4bf32943569c554ca49
k0rzun1n <korzuninsl@mail.ru>
2024-12-19 23:54:47 +0500

    Update 1115-print-foobar-alternately.go

--- LeetCode/1115-print-foobar-alternately.go
+++ LeetCode/1115-print-foobar-alternately.go
@@ -4,32 +4,29 @@ package main
 
 type FooBar struct {
 	n    int
-	ping chan bool
-	pong chan bool
+	sync chan bool
 }
 
 func NewFooBar(n int) *FooBar {
 	nfb := FooBar{n: n}
-	nfb.ping = make(chan bool, 1)
-	nfb.pong = make(chan bool, 1)
-	nfb.ping <- true
+	nfb.sync = make(chan bool, 1)
 	return &nfb
 }
 
 func (fb *FooBar) Foo(printFoo func()) {
 	for i := 0; i < fb.n; i++ {
-		<-fb.ping
 		// printFoo() outputs "foo". Do not change or remove this line.
 		printFoo()
-		fb.pong <- true
+		fb.sync <- true
+		<-fb.sync
 	}
 }
 
 func (fb *FooBar) Bar(printBar func()) {
 	for i := 0; i < fb.n; i++ {
-		<-fb.pong
 		// printBar() outputs "bar". Do not change or remove this line.
+		<-fb.sync
 		printBar()
-		fb.ping <- true
+		fb.sync <- true
 	}
 }
